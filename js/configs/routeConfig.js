157811aab64f5109b3d1c8755df018f843431de6
luketevl <luketevl@gmail.com>
2016-03-29 19:59:13 -0300

    Instalador

--- js/configs/routeConfig.js
+++ js/configs/routeConfig.js
@@ -6,6 +6,18 @@ angular.module('gerenciadorErp').config(function($routeProvider){
     controller: 'principalController',
   });
 
+  // ROTA NOVO SISTEMA
+  $routeProvider.when('/checkout', {
+    templateUrl: 'views/checkout/index.html',
+    controller: 'checkoutController',
+  });
+
+  // ROTA LISTAGEM DE SISTEMAS
+  $routeProvider.when('/lista_sistemas', {
+    templateUrl: 'views/lista_sistemas.html',
+    controller: 'checkoutController',
+  });
+
 
   // ROTA PADRAO para quando nao existir nenhuma
   $routeProvider.otherwise({redirectTo: '/principal'});
